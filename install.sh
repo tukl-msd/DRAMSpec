d88ee1c74f719cb905e4e87ee104b8677f2805d8
Matthias Jung <jungma@eit.uni-kl.de>
2014-04-24 13:27:23 +0200

    install.sh script modified to ignore certificates

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget https://rapidjson.googlecode.com/files/rapidjson-0.11.zip
+wget --no-check-certificate https://rapidjson.googlecode.com/files/rapidjson-0.11.zip
 unzip rapidjson-0.11.zip
